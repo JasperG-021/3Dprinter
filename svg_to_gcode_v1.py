b6d34273f2159718e0decdff09c5113e73189175
JasperG021 <kakuyasukuni@gmail.com>
2026-07-07 01:20:03 +0900

    建立文件管理

--- svg_to_gcode_v1.py
+++ svg_to_gcode_v1.py
@@ -72,7 +72,7 @@ def svg_to_safe_curved_gcode(svg_file_path, gcode_file_path):
 
 # --- 测试运行 ---
 file_date = str(datetime.datetime.today()).split()[0].replace('-', '')
-svg_file = "/Users/jasperg/Documents/Codes/Python/3Dprinter/test_20260706_1.svg"
-gcode_file = "/Users/jasperg/Documents/Codes/Python/3Dprinter/test_"+file_date+".gcode"
+svg_file = "/Users/jasperg/Documents/Codes/Python/3Dprinter/resources/test_20260706.svg"
+gcode_file = "/Users/jasperg/Documents/Codes/Python/3Dprinter/gcode_result/test_"+file_date+".gcode"
 # print(gcode_file)
 svg_to_safe_curved_gcode(svg_file, gcode_file)
