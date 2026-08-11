71e9fe53ad4c7f645c94636af93f7f8c12b2bd0f
JasperG-021 <kakuyasukuni@gmail.com>
2026-08-11 12:22:36 +0000

    不知道修改了什么，从这里开始

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 import datetime
-from main.svg_to_gcode_v1 import svg_to_safe_curved_gcode
+from main.svg_to_gcode import svg_to_safe_curved_gcode
 
 file_date = str(datetime.datetime.today()).split()[0].replace('-', '')
-svg_file = "resources/test_20260706.svg"
+svg_file = "resources/test_20260727.svg"
 gcode_file = "gcode_result/test_"+file_date+".gcode"
 # print(gcode_file)
 svg_to_safe_curved_gcode(svg_file, gcode_file)
